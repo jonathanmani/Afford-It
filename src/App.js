b15a4b3d2b8d1167e58c9d89b4b9bac2011923d8
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2023-01-27 15:13:02 -0500

    added some dummy data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from "./components/Budget";
+import ExpenseTotal from "./components/ExpenseTotal";
+import Remaining from "./components/Remaining";
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className="container">
+      <h1 className="mt-3">My Budget Planner</h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <Budget />
+        </div>
+        <div className="col-sm">
+          <Remaining />
+        </div>
+        <div className="col-sm">
+          <ExpenseTotal />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
