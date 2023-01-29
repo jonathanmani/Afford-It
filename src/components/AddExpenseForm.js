61250c3c897bcb898abbb8dc6bd8f6dc1a31e679
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2023-01-29 11:56:32 -0500

    added the dispatch action

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
 
 const AddExpenseForm = () => {
+  const { dispatch } = useContext(AppContext);
   const [name, setName] = useState("");
   const [cost, setCost] = useState("");
 
-  const onSubmit = (event) => {};
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const expense = {
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+    };
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
   return (
     <form onSubmit={onSubmit}>
       <div className="row">
