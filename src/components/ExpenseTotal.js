f19ec6b5d5377b2d5eab836df01adfcb4d443007
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2023-01-30 12:17:02 -0500

    updated the ExpenseTotal component

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseTotal = () => {
+  const { expenses } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
   return (
     <div className="alert alert-primary">
-      <span>Spent So Far: $1000</span>
+      <span>Spent So Far: ${totalExpenses}}</span>
     </div>
   );
 };
